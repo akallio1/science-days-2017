5cd933e3061b2f3feee50544679f2152016bb356
Aleksi Kallio <aleksi.kallio@csc.fi>
2017-01-12 13:19:18 +0200

    Fix URL

--- download.sh
+++ download.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-base="https://github.com/akallio1/science-days-2017/master/notebooks"
-
-wget "$base/tieteen-paivat-2017.ipynb"
+wget "https://raw.githubusercontent.com/akallio1/science-days-2017/master/tieteen-paivat-2017.ipynb"
