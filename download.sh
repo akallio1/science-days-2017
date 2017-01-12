4d6460dcd0a8201a674c9d8f9663c6efd6e43c37
Aleksi Kallio <aleksi.kallio@csc.fi>
2017-01-12 21:57:23 +0200

    Restore scikit-learn

--- download.sh
+++ download.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
+# Libs
+pip install scikit-learn
+
+# Data
 wget "https://raw.githubusercontent.com/akallio1/science-days-2017/master/tieteen-paivat-2017.ipynb"
+
+# Self-remove this init script
 rm download.sh
